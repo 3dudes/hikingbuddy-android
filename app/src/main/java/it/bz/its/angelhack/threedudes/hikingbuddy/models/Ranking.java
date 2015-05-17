8189a657eb232a9dc416ae1f688661f512b7d56c
Victor ADASCALITEI <victor@rrsolutions.it>
2015-05-17 09:12:32 +0200

    Second stage commit

---
+++ app/src/main/java/it/bz/its/angelhack/threedudes/hikingbuddy/models/Ranking.java
@@ -0,0 +1,13 @@
+package it.bz.its.angelhack.threedudes.hikingbuddy.models;
+
+import java.util.Date;
+
+public class Ranking {
+    int id;
+    Date startedAt;
+    Date completedAt;
+    String status;
+    int missionId;
+    int score;
+    User user;
+}
---
+++ app/src/main/java/it/bz/its/angelhack/threedudes/hikingbuddy/models/RankingResponse.java
@@ -0,0 +1,7 @@
+package it.bz.its.angelhack.threedudes.hikingbuddy.models;
+
+import java.util.List;
+
+public class RankingResponse {
+    List<Ranking> ranking;
+}
